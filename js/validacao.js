f3c3827a2a0a8a2d8b0a6ec87fd37ade353434d3
GuiSousa2910 <guilherme.agostinho@sptech.school>
2024-05-09 17:21:12 -0300

    IA 90% pronta

---
+++ js/validacao.js
@@ -0,0 +1,23 @@
+var espressaoBooleana = false;
+let idBotao = document.getElementById('sendButton');
+
+function abilitar() {
+    var informacao = userInput.value;
+
+    if (informacao == '') {
+        espressaoBooleana = false;
+    } else {
+        espressaoBooleana = true;
+    }
+}
+
+function verificar() {
+    if (espressaoBooleana) {
+        document.getElementById('sendButton').removeAttribute('disabled');
+    }
+    else {
+        document.getElementById('sendButton').setAttribute('disabled', true);
+    }
+}
+
+
--- server.js
+++ server.js
@@ -12,18 +12,16 @@ const MODEL_NAME = "gemini-1.0-pro";
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
-const generationConfig = {
-  temperature: 0.5,
-  topK: 0,
-  topP: 0.95,
-  maxOutputTokens: 8192,
+generationConfig = {
+  temperature: 0,
+  topK: 0,  
+  topP: 0.9, 
+  maxOutputTokens: 20000
 };
 
 const safetySettings = [
   { category: HarmCategory.HARM_CATEGORY_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
-  { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
-  { category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE},
-  { category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE}
+  { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE }
 ];
 
 app.use(cors());
@@ -32,12 +30,23 @@ app.use(serveStatic(__dirname));
 
 app.get('/generate-text', async (req, res) => {
   const text = req.query.text;
+  const prompt = `Por favor, forneça uma resposta de tamanho mediano para a seguinte pergunta sobre o jogo 'Stardew Valley': ${text}`
   const chat = model.startChat({ generationConfig, safetySettings });
-  const result = await chat.sendMessage(text);
+  const result = await chat.sendMessage(prompt);
   const response = result.response;
   res.send(response.text());
 });
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`
+  
+    ##       ####    ####               ####    #######   ##   ##   ####    ##   ##   ####
+   ####     ##  ##    ##               ##  ##   ##        ### ###    ##     ###  ##    ##
+  ##  ##    ##  ##    ##              ##        ##        #######    ##     #### ##    ##
+  ##  ##    #####     ##     ######   ##        ####      ## # ##    ##     ## ####    ##
+  ######    ##        ##              ##  ###   ##        ##   ##    ##     ##  ###    ##
+  ##  ##    ##        ##               ##  ##   ##        ##   ##    ##     ##   ##    ##
+  ##  ##   ####      ####               #####   #######   ##   ##   ####    ##   ##   ####
+  \n\n\n      
+  Acesse o caminho a seguir para visualizar .: http://localhost:${port}/ia.html :. \n\n`)
 });
